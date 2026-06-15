90085ba5820de06308db703d57e176a16a68a2f7
Ee Lin Goh <ee.lin.goh1@ibm.com>
2026-06-15 15:48:43 +0800

    added sample crypto file

---
+++ src/main/java/org/springframework/samples/petclinic/CryptoSample.java
@@ -0,0 +1,26 @@
+package entity;
+
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+import java.security.MessageDigest;
+import java.util.Base64;
+
+public class CryptoSample {
+
+    public static String sha256(String input) throws Exception {
+        MessageDigest digest = MessageDigest.getInstance("SHA-256");
+        byte[] hash = digest.digest(input.getBytes("UTF-8"));
+        return Base64.getEncoder().encodeToString(hash);
+    }
+
+    public static SecretKey generateAesKey() throws Exception {
+        KeyGenerator keyGenerator = KeyGenerator.getInstance("AES");
+        keyGenerator.init(256);
+        return keyGenerator.generateKey();
+    }
+
+    public static void main(String[] args) throws Exception {
+        System.out.println("SHA-256: " + sha256("hello cbomkit"));
+        System.out.println("AES key algorithm: " + generateAesKey().getAlgorithm());
+    }
+}
